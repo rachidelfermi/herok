da2976913143c6b7f6b88626e2b67210e9ecba84
rachidelfe <34278755+rachidelfe@users.noreply.github.com>
2020-09-06 22:56:22 +0100

    coming soon

--- app.py
+++ app.py
@@ -28,34 +28,8 @@ print('Model loaded. Check http://127.0.0.1:5000/')
 def home():
     # Main page
     return render_template('index.html')
-@app.route('/Submission.html')
-def submit():
-    return render_template('Submission.html')
-@app.route('/index.html')
-def index():
-    return render_template('index.html')
-@app.route('/Download.html')
-def Download():
-    return render_template('Download.html')
-@app.route('/Results.html')
-def results():
-    return render_template('Results.html')
-@app.route('/Help.html')
-def Help():
-    return render_template('Help.html')
-@app.route('/Department.html')
-def departemnt():
-    return render_template('Department.html')
-@app.route('/predict', methods=['GET', 'POST'])
-def predict():
 
-    if request.method == 'POST':
 
-        # Get the file from post request
-        
-        print(
-            'zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz')
-        
 
 
 if __name__ == '__main__':
